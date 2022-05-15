4353710f928ebdcea756ba76c0c2cdf846136ffb
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-05-15 18:34:14 -0500

    commit 516

---
+++ leetcode/0516-Longest-Palindromic-Subsequence/0516-Longest-Palindromic-Subsequence.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import "math"
+
+// dp[i][j] := length of longest padlindrome subseq of s[i...j]
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+
+	// prepend s with a placeholder
+	s = "#" + s
+
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := n; i >= 1; i-- {
+		for j := 1; j <= n; j++ {
+			if i > j {
+				dp[i][j] = 0
+			} else if i == j {
+				dp[i][j] = 1
+			} else {
+				if s[i] == s[j] {
+					dp[i][j] = 2 + dp[i+1][j-1]
+				} else {
+					dp[i][j] = int(math.Max(float64(dp[i+1][j]), float64(dp[i][j-1])))
+				}
+			}
+		}
+	}
+	return dp[1][n]
+}
---
+++ leetcode/0516-Longest-Palindromic-Subsequence/0516-Longest-Palindromic-Subsequence_test.go
@@ -0,0 +1,56 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question516 struct {
+	para516
+	ans516
+}
+
+// para 是参数
+type para516 struct {
+	s1 string
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans516 struct {
+	one int
+}
+
+func Test_Problem516(t *testing.T) {
+
+	qs := []question516{
+
+		{
+			para516{"character"},
+			ans516{one: 5},
+		},
+		{
+			para516{"bbbab"},
+			ans516{one: 4},
+		},
+		{
+			para516{"cbbd"},
+			ans516{one: 2},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 97------------------------\n")
+
+	for _, q := range qs {
+		expect, input := q.ans516, q.para516
+		testname := fmt.Sprintf("%v", input.s1)
+		t.Run(testname, func(t *testing.T) {
+			ans := longestPalindromeSubseq(input.s1)
+			if ans != expect.one {
+				t.Errorf("got %v, want %v", ans, expect.one)
+			}
+			fmt.Printf("【input】:%v       【output】:%v\n", input, ans)
+		})
+	}
+	fmt.Printf("\n\n\n")
+}
