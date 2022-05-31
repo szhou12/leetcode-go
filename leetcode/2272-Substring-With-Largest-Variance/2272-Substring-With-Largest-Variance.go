f0e78d88942519148b3c8da470c01ad8140b37f7
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-05-30 21:45:33 -0500

    added comments

--- leetcode/2272-Substring-With-Largest-Variance/2272-Substring-With-Largest-Variance.go
+++ leetcode/2272-Substring-With-Largest-Variance/2272-Substring-With-Largest-Variance.go
@@ -9,6 +9,8 @@ func largestVariance(s string) int {
 	}
 
 	ret := 0
+	// Try out all possible distinct letter pairs;
+	// treat one as 1, the other as -1, treat all remaining letters as 0
 	for i := 0; i < 26; i++ {
 		for j := 0; j < 26; j++ {
 			if count[i] == 0 || count[j] == 0 {
@@ -26,6 +28,7 @@ func largestVariance(s string) int {
 					nums[k] = -1
 				}
 			}
+			// helper() implements max sum subarray (057) from left-to-right and from right-to-left
 			ret = max(ret, helper(nums))
 
 		}
@@ -36,16 +39,22 @@ func largestVariance(s string) int {
 
 func helper(nums []int) int {
 	n := len(nums)
+	ret := 0
+
 	dp1 := make([]int, n)
 	dp1[0] = nums[0]
+	// dp1: from left-to-right
 	for i := 1; i < n; i++ {
 		dp1[i] = max(dp1[i-1]+nums[i], nums[i])
 	}
 
 	curSum := 0
-	ret := 0
+	// dp2: from right-to-left
 	for i := n - 1; i >= 0; i-- {
 		curSum = max(curSum+nums[i], nums[i])
+
+		// ONLY update results at i-th element where nums[i] == -1
+		// B/c we only consider subarrays that have at least one -1
 		if nums[i] == -1 {
 			ret = max(ret, dp1[i]+curSum-nums[i])
 		}
--- leetcode/2272-Substring-With-Largest-Variance/README.md
+++ leetcode/2272-Substring-With-Largest-Variance/README.md
@@ -1,1 +1,11 @@
 # [2272. Substring With Largest Variance](https://leetcode.com/problems/substring-with-largest-variance/)
+
+## Note:
+
+Neet to use two DP arrays:
+
+DP1[i]: the largest sum subarray ending at i (left-to-right)
+
+DP2[i]: the largest sum subarray starting at i (right-to-left)
+
+result = DP1[i] + DP2[i] - nums[i]
