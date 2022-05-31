0d4cd738cc0ca07a795ee993dce249a53b735bb9
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-05-30 21:24:59 -0500

    commit 2272

---
+++ leetcode/2272-Substring-With-Largest-Variance/2272-Substring-With-Largest-Variance.go
@@ -0,0 +1,62 @@
+package leetcode
+
+func largestVariance(s string) int {
+	n := len(s)
+
+	count := make([]int, 26)
+	for _, char := range s {
+		count[char-'a'] += 1
+	}
+
+	ret := 0
+	for i := 0; i < 26; i++ {
+		for j := 0; j < 26; j++ {
+			if count[i] == 0 || count[j] == 0 {
+				continue
+			}
+			if i == j {
+				continue
+			}
+
+			nums := make([]int, n)
+			for k := 0; k < n; k++ {
+				if int(s[k]) == 'a'+i {
+					nums[k] = 1
+				} else if int(s[k]) == 'a'+j {
+					nums[k] = -1
+				}
+			}
+			ret = max(ret, helper(nums))
+
+		}
+	}
+	return ret
+
+}
+
+func helper(nums []int) int {
+	n := len(nums)
+	dp1 := make([]int, n)
+	dp1[0] = nums[0]
+	for i := 1; i < n; i++ {
+		dp1[i] = max(dp1[i-1]+nums[i], nums[i])
+	}
+
+	curSum := 0
+	ret := 0
+	for i := n - 1; i >= 0; i-- {
+		curSum = max(curSum+nums[i], nums[i])
+		if nums[i] == -1 {
+			ret = max(ret, dp1[i]+curSum-nums[i])
+		}
+	}
+
+	return ret
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
