deef532f3d989fbd3b322224b238156e059b9518
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-06-22 16:11:04 -0500

    commit subsetssizek

---
+++ otheralgorithms/Subsets-Of-Size-K.go
@@ -0,0 +1,44 @@
+package otheralgorithms
+
+/*
+ * All Subsets of Size K
+ *
+ * Given a []int,
+ * return a list containing all subsets of size K.
+ *
+ * Assumptions
+ * There are no duplicate elements in the original set.
+ *
+ */
+func subsetsOfSizeK(nums []int, k int) [][]int {
+	var result [][]int
+	var subset []int
+
+	dfs(nums, 0, subset, &result, k)
+	return result
+}
+
+func dfs(nums []int, index int, subset []int, result *[][]int, k int) {
+	// base case
+	if k == len(subset) {
+		subsetCopy := make([]int, len(subset))
+		copy(subsetCopy, subset)
+		*result = append(*result, subsetCopy)
+		return
+	}
+
+	// Early stopping!!!
+	// It's possible that index is already out of bound (checked all letters) but still len(subset) < k
+	// In this case, we should terminate recursion as long as index == len(subset)
+	if index == len(subset) {
+		return
+	}
+
+	// branch 1: add current element
+	subset = append(subset, index+1)
+	dfs(nums, index+1, subset, result, k)
+	// backtracking
+	subset = subset[:len(subset)-1]
+	// branch 2: not add current element
+	dfs(nums, index+1, subset, result, k)
+}
