1c7f33b09b0fc38683ec1273ebedd7ecb333b3e7
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-07-04 20:39:06 -0500

    commit 2318

--- leetcode/2318-Number-of-Distinct-Roll-Sequences/2318-Number-of-Distinct-Roll-Sequences.go
+++ leetcode/2318-Number-of-Distinct-Roll-Sequences/2318-Number-of-Distinct-Roll-Sequences.go
@@ -1,6 +1,69 @@
 package leetcode
 
+// DP - Optimal solution
+func distinctSequences(n int) int {
+	// Edge case: n == 1
+	if n == 1 {
+		return 6
+	}
+
+	modulo := int64(1e9 + 7)
+	dp := make([][][]int, n+1)
+
+	for i := 0; i <= n; i++ {
+		dp[i] = make([][]int, 6+1)
+		for a := 0; a <= 6; a++ {
+			dp[i][a] = make([]int, 6+1)
+		}
+	}
+
+	count := 0
+	for a := 1; a <= 6; a++ {
+		for b := 1; b <= 6; b++ {
+			if a != b && gcd(a, b) == 1 {
+				dp[2][a][b] += 1
+				count += 1
+			}
+		}
+	}
+
+	// Edge case: n == 2
+	if n == 2 {
+		return count
+	}
+
+	// DP
+	for i := 3; i <= n; i++ {
+		for a := 1; a <= 6; a++ {
+			for b := 1; b <= 6; b++ {
+				for c := 1; c <= 6; c++ {
+					if a != b && gcd(a, b) == 1 && a != c {
+						dp[i][b][a] = int(int64(dp[i][b][a]+dp[i-1][c][b]) % modulo)
+					}
+				}
+			}
+		}
+	}
+
+	// Result
+	res := 0
+	for a := 1; a <= 6; a++ {
+		for b := 1; b <= 6; b++ {
+			res = int(int64(res+dp[n][b][a]) % modulo)
+		}
+	}
+	return res
+
+}
+
 // DFS - can pass ONLY when n small
+// n levels
+// each level at most 6 branches bc 6-sided dice
+//     {}
+// / | | | | \
+// 1 2 3 4 5  6
+// /
+// 2-6
 func distinctSequences_DFS(n int) int {
 	var cur []int
 	var result [][]int
@@ -40,6 +103,7 @@ func dfs(n int, cur []int, result *[][]int) {
 	}
 }
 
+// Euclidean Algorithm
 func gcd(a int, b int) int {
 	for b != 0 {
 		t := b
--- leetcode/2318-Number-of-Distinct-Roll-Sequences/README.md
+++ leetcode/2318-Number-of-Distinct-Roll-Sequences/README.md
@@ -12,3 +12,36 @@ Since it asks for **total number** of distinct sequences possible, the most intu
 3) Pruning: At each level, continue DFS only if two required conditions are satisfied
 
 Time complexity = $O(6^n)$. Clearly, too expensive.
+
+### 3-D DP (Optimal Solution)
+Define $DP[i][b][a]$ as total number of possible sequences when rolling $i$ times where the $i-1$-th value is $b$ and $i$-th value is $a$. (`X X X X b a`)
+
+Reccurence:
+
+$DP[i][b][a] = \sum $DP[i-1][c][b]$$ where $1\leq a,b,c \leq 6$ and $gcd(a,b)=1$ and $a\neq b$ and $a\neq c$.
+
+Base cases:
+
+if $n=1$, only $6$ possible ways to roll a dice once.
+
+if $n=2$, all pairs $(x,y)$ where $x \neq y$ and $gcd(x,y) == 1$.
+
+Result:
+
+$\sum DP[n][X][X]$ where $1\leq X \leq 6$.
+
+Time complexity = $O(6^3\times n)$
+
+### WHY 3-D DP?
+`DP[i]` 表示 扔 i 次 的所有可行方法。 但好像不够，怎么知道第i次扔到的数能不能满足条件呢？`DP[i]` and `DP[i-1]` 没法确定是不是合法的
+
+那么，这样呢？
+
+`DP[i][a]` 表示 扔 i 次而且第i次扔到a 的所有可行方法, 这样我们就可以显性地建立 i-1 和 i 次的联系: `DP[i][a]` and `DP[i-1][b]`
+
+但是，题目还要求第i次扔到的数和第i-2次扔到的数不能相等。似乎，二维DP又不够用了。那么，三维呢？
+
+`DP[i][b][a]` 表示 扔 i 次 而且 第i-1次扔到b & 第i次扔到a 的所有可行方法, 这样我们就可以显性地建立 i-1 和 i 次的联系 又能 考虑到 i-2 和 i的要求： `DP[i][b][a]` and `DP[i-1][c][b]`.
+
+### Resource
+[【每日一题】LeetCode 2318. Number of Distinct Roll Sequences](https://www.youtube.com/watch?v=7SZBLIga_-s&ab_channel=HuifengGuan)
