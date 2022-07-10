f72ffea2756fc5a8cc95562d9e6d998f91b9944c
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-07-09 22:39:15 -0500

    commit 2332

---
+++ leetcode/2332-The-Latest-Time-to-Catch-a-Bus/2332-The-Latest-Time-to-Catch-a-Bus.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import "sort"
+
+func latestTimeCatchTheBus(buses []int, passengers []int, capacity int) int {
+	sort.Slice(buses, func(i, j int) bool {
+		return buses[i] < buses[j]
+	})
+	sort.Slice(passengers, func(i, j int) bool {
+		return passengers[i] < passengers[j]
+	})
+
+	res := -1
+	n := len(passengers)
+	j := 0 // pointer to passenger
+	for i := 0; i < len(buses); i++ {
+		cap := capacity
+
+		// case 1: 看哪些乘客可以赶上 i-th bus, 挤掉乘客j, 自己上位
+		for j < n && passengers[j] <= buses[i] && cap > 0 {
+			if j == 0 || (j >= 1 && passengers[j]-1 != passengers[j-1]) {
+				// 挤掉 j-th passenger, 自己上位
+				res = max(res, passengers[j]-1)
+			}
+			j++
+			cap--
+		}
+
+		// case 2: 如果 i-th bus 还可以容纳更多人, 紧着bus出发时间来安排我自己的时间
+		if cap > 0 {
+			if j == 0 || (j >= 1 && passengers[j-1] != buses[i]) {
+				res = max(res, buses[i])
+			}
+		}
+	}
+
+	return res
+}
+
+// max 可以省略，因为一开始就sort过了, 所以 res一定是单调递增的
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ leetcode/2332-The-Latest-Time-to-Catch-a-Bus/README.md
@@ -0,0 +1,6 @@
+# [2332. The Latest Time to Catch a Bus](https://leetcode.com/problems/the-latest-time-to-catch-a-bus/)
+
+## Solution idea
+Greedy Algorithm with sorting
+
+Time complexity = $O(n\times m)$ where $n = $ number of buses, $m = $ number of passengers
