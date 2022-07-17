8bd0f781fb747f49b6d98adb53b334762bdd247c
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-07-16 23:09:01 -0500

    commit 1774

---
+++ leetcode/1774-Closest-Dessert-Cost/1774-Closest-Dessert-Cost.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import "math"
+
+func closestCost(baseCosts []int, toppingCosts []int, target int) int {
+	m := len(toppingCosts)
+
+	res := math.MaxInt
+	resDiff := math.MaxInt
+
+	for _, base := range baseCosts {
+		for state := 0; state < pow(3, m); state++ {
+			s := state
+			topping := 0 // topping costs for current state
+			for i := 0; i < m; i++ {
+				topping += (s % 3) * toppingCosts[i]
+				s /= 3
+			}
+
+			if abs(base+topping-target) < resDiff {
+				res = base + topping
+				resDiff = abs(base + topping - target)
+			} else if abs(base+topping-target) == resDiff && (base+topping) < target {
+				res = base + topping
+			}
+		}
+	}
+
+	return res
+}
+
+func pow(a int, x int) int {
+	res := math.Pow(float64(a), float64(x))
+	return int(res)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
---
+++ leetcode/1774-Closest-Dessert-Cost/README.md
@@ -0,0 +1,11 @@
+# [1774. Closest Dessert Cost](https://leetcode.com/problems/closest-dessert-cost/)
+
+## Solution idea
+Brute force
+
+**Key point**: 用三进制表示每一种 topping 选的量：0, 1, 2
+
+Time complexity = $O(n\times 3^m)$
+
+## Resources
+[【每日一题】LeetCode 1774. Closest Dessert Cost](https://www.youtube.com/watch?v=HipFluaR1RA&ab_channel=HuifengGuan)
