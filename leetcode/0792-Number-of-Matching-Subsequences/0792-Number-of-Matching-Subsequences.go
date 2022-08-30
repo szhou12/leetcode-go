4a1d87aae1f797d5845bacd70e5810ce23fa8eb8
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-08-30 17:08:11 -0500

    add 792

--- leetcode/0792-Number-of-Matching-Subsequences/0792-Number-of-Matching-Subsequences.go
+++ leetcode/0792-Number-of-Matching-Subsequences/0792-Number-of-Matching-Subsequences.go
@@ -1,11 +1,74 @@
 package leetcode
 
+// Optimal Solution - Binary Search
+// Use 0392 - binary search as helper function
+func numMatchingSubseq(s string, words []string) int {
+	// Step 1: pre-process s
+	dict := make([][]int, 26)
+	for index, char := range s {
+		dict[int(char-'a')] = append(dict[int(char-'a')], index)
+	}
+
+	res := 0
+	// Step 2: binary search for each word in words
+	for _, word := range words {
+		sPointer := 0 // Pointer for s, the target string
+
+		// Pointer for current word: it's used to tell whether we can
+		// successfully iter all chars in word. If so, we increment res.
+		// If break at any point in between, word is NOT a subseq.
+		wordPointer := 0
+		for ; wordPointer < len(word); wordPointer++ {
+			char := word[wordPointer]
+
+			// Case 1: this char doesn't even exist in dict for s
+			if len(dict[int(char-'a')]) == 0 {
+				break
+			}
+
+			// Case 2: binary search
+			pos := leftBound(dict[int(char-'a')], sPointer)
+			if pos == -1 {
+				break
+			}
+			sPointer = dict[int(char-'a')][pos] + 1
+		}
+
+		// word is a subseq ONLY IF we successfully iter all chars in word
+		if wordPointer == len(word) {
+			res++
+		}
+	}
+
+	return res
+}
+
+func leftBound(array []int, target int) int {
+	left := 0
+	right := len(array) - 1
+
+	for left < right {
+		mid := left + (right-left)/2
+		if array[mid] >= target {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	if array[left] < target {
+		return -1
+	}
+
+	return left
+}
+
 // Solution - Brute force
-// Use 0392 as helper function
+// Use 0392 - greedy algo as helper function
 func numMatchingSubseq_BF(s string, words []string) int {
 	count := 0
 	for _, t := range words {
-		if isSubseq(s, t) {
+		if isSubseq_Greedy(s, t) {
 			count++
 		}
 	}
@@ -14,7 +77,7 @@ func numMatchingSubseq_BF(s string, words []string) int {
 }
 
 // check if t is subseq of s
-func isSubseq(s string, t string) bool {
+func isSubseq_Greedy(s string, t string) bool {
 	// edge cases
 	// if s empty, t empty => true
 	// if s empty, t not => false
---
+++ leetcode/0792-Number-of-Matching-Subsequences/README.md
@@ -0,0 +1,17 @@
+# [792. Number of Matching Subsequences](https://leetcode.com/problems/number-of-matching-subsequences/)
+
+## Solution ideas
+
+### Brute Force
+
+Loop + Greedy Algorithm of [392. Is Subsequence](https://leetcode.com/problems/is-subsequence/)
+
+### Binary Search - Optimal Solution
+
+Loop + Binary Search of [392. Is Subsequence](https://leetcode.com/problems/is-subsequence/)
+
+Time complexity = $O(\sum_{k=1}^m W_k\log n)$ where $W_k=$ length of $k$-th `word` in `words`, and $n=$ length of target string `s`
+
+## Resource
+
+[二分查找高效判定子序列](https://labuladong.github.io/algo/4/33/133/)
