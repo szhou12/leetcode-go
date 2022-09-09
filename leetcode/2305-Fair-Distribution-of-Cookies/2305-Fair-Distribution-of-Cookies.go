30e73f0ff11b57bbf281ad2f79473f415d071aab
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-09-09 14:11:04 -0500

    update 2305

--- leetcode/2305-Fair-Distribution-of-Cookies/2305-Fair-Distribution-of-Cookies.go
+++ leetcode/2305-Fair-Distribution-of-Cookies/2305-Fair-Distribution-of-Cookies.go
@@ -2,6 +2,55 @@ package leetcode
 
 import "math"
 
+// Optimized Solution - Binary Search + DFS
+func distributeCookies_BSDFS(cookies []int, k int) int {
+	plan := make([]int, 8)
+
+	// Binary Search on max amount of cookies a person can have
+	// mid := upper limit
+	left := 1
+	right := math.MaxInt
+	for left < right {
+
+		// need to reset plan for every trial
+		for i := 0; i < 8; i++ {
+			plan[i] = 0
+		}
+
+		mid := left + (right-left)/2
+		if dfs2(cookies, k, 0, &plan, mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
+func dfs2(cookies []int, k int, index int, plan *[]int, limit int) bool {
+	// Base case
+	if index == len(cookies) {
+		return true
+	}
+
+	for person := 0; person < k; person++ {
+		// if current person exceeds cookies limit, skip
+		if (*plan)[person]+cookies[index] > limit {
+			continue
+		}
+		(*plan)[person] += cookies[index]
+		if dfs2(cookies, k, index+1, plan, limit) {
+			return true
+		}
+		// if giving current person the index-th cookie results in none of workable soln, take it away
+		(*plan)[person] -= cookies[index]
+	}
+
+	return false
+}
+
+// Brute force - DFS
 func distributeCookies(cookies []int, k int) int {
 	res := math.MaxInt
 	plan := make([]int, 8)
--- leetcode/2305-Fair-Distribution-of-Cookies/README.md
+++ leetcode/2305-Fair-Distribution-of-Cookies/README.md
@@ -7,3 +7,15 @@
 DFS暴力解
 
 每一层是当前cookie。 每一个分支是一个人，总共`k`个分支。
+
+Time complexity = $O(k^n)$
+
+### Binary Search + DFS
+
+**思路**: 如果**直接找答案** (DFS暴力解) 不够efficient，那么，就反其道而行之，**猜答案**.
+
+**猜答案**的方法：**Binary Search** - 对每个人最多能拿到的数量进行猜测，如果当前数量可行，可能是答案，再往低了猜。
+
+## Resource
+
+[【每日一题】LeetCode 2305. Fair Distribution of Cookies](https://www.youtube.com/watch?v=tAIzu_MWa8U&t=840s)
