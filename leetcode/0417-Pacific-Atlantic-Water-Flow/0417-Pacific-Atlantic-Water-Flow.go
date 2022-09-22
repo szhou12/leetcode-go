0bab91b48821b4c96d5696f6e16bf670de93dbee
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-09-22 16:31:55 -0500

    add 417

---
+++ leetcode/0417-Pacific-Atlantic-Water-Flow/0417-Pacific-Atlantic-Water-Flow.go
@@ -0,0 +1,58 @@
+package leetcode
+
+import "math"
+
+// DFS Solution - 逆向思维
+func pacificAtlantic(heights [][]int) [][]int {
+	rows, cols := len(heights), len(heights[0])
+	pacific, atlantis := make([][]bool, rows), make([][]bool, rows)
+	for i := 0; i < rows; i++ {
+		pacific[i] = make([]bool, cols)
+		atlantis[i] = make([]bool, cols)
+	}
+
+	// pacific遍历O(rows*cols), atlantis遍历O(rows*cols)
+	for i := 0; i < rows; i++ {
+		dfs(heights, i, 0, &pacific, math.MinInt)       // 左边界回流
+		dfs(heights, i, cols-1, &atlantis, math.MinInt) // 右边界回流
+	}
+	for j := 0; j < cols; j++ {
+		dfs(heights, 0, j, &pacific, math.MinInt)       // 上边界回流
+		dfs(heights, rows-1, j, &atlantis, math.MinInt) // 下边界回流
+	}
+
+	res := make([][]int, 0)
+	// O(rows*cols)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if pacific[i][j] && atlantis[i][j] {
+				res = append(res, []int{i, j})
+			}
+		}
+	}
+	return res
+}
+
+func dfs(heights [][]int, x int, y int, visited *[][]bool, prevHeight int) {
+	// 3 Cases of Early Stopping
+	// Case 1: out of bound
+	if x < 0 || x >= len(heights) || y < 0 || y > len(heights[0]) {
+		return
+	}
+	// Case 2: 上一层height > 当前height，无法回流至当前cell
+	if prevHeight > heights[x][y] {
+		return
+	}
+	// Case 3: 当前cell已经标记为可行解
+	if (*visited)[x][y] {
+		return
+	}
+
+	(*visited)[x][y] = true
+
+	// recursion: 分别向四个方向走
+	dfs(heights, x+1, y, visited, heights[x][y])
+	dfs(heights, x-1, y, visited, heights[x][y])
+	dfs(heights, x, y+1, visited, heights[x][y])
+	dfs(heights, x, y-1, visited, heights[x][y])
+}
---
+++ leetcode/0417-Pacific-Atlantic-Water-Flow/README.md
@@ -0,0 +1,39 @@
+# [417. Pacific Atlantic Water Flow](https://leetcode.com/problems/pacific-atlantic-water-flow/)
+
+## Solution idea
+
+### DFS
+
+**Key 1**: 用DFS暴力搜索每个cell是否可以通向 pacific 和 atlantis
+
+**Key 2**: 逆向思维 - 从棋盘的四个边界分别出发回流，看哪些cell可以回流到 (下一个cell比当前cell更高才能回流到)
+
+时间复杂度分析：
+
+根据 Early Stopping Case 3, 其实 有visited函数记录 走过的节点，而走过的节点是不会再走第二次的。
+
+所以 调用dfs函数，**只要参数传入的是 数组pacific，那么地图中 每一个节点其实就遍历一次，无论你调用多少次。**
+
+同理，调用 dfs函数，**只要参数传入的是 数组atlantic，地图中每个节点也只会遍历一次。**
+
+这段代码，pacific遍历花费 $O(m*n)$, atlantis遍历花费 $O(m*n)$; 所以，总共花费$O(2*m*n)$
+```
+	for i := 0; i < rows; i++ {
+		dfs(heights, i, 0, &pacific, math.MinInt)       // 左边界回流
+		dfs(heights, i, cols-1, &atlantis, math.MinInt) // 右边界回流
+	}
+	for j := 0; j < cols; j++ {
+		dfs(heights, 0, j, &pacific, math.MinInt)       // 上边界回流
+		dfs(heights, rows-1, j, &atlantis, math.MinInt) // 下边界回流
+	}
+```
+
+Time complexity = $O(2*m*n) + O(m*n)$ = $O(m*n)$
+
+## Resrouces
+
+[参考答案1](https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0417.%E5%A4%AA%E5%B9%B3%E6%B4%8B%E5%A4%A7%E8%A5%BF%E6%B4%8B%E6%B0%B4%E6%B5%81%E9%97%AE%E9%A2%98.md)
+
+[参考答案2](https://github.com/halfrost/LeetCode-Go/tree/master/leetcode/0417.Pacific-Atlantic-Water-Flow)
+
+[参考答案3](https://github.com/wisdompeak/LeetCode/tree/master/DFS/417.Pacific-Atlantic-Water-Flow)
