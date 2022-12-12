afa93a76f09cb9f15f20f1aee0643295e696449c
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-12-12 17:37:19 -0600

    add 395 divide conquer

---
+++ leetcode/0395-Longest-Substring-with-At-Least-K-Repeating-Characters/0395-Longest-Substring-with-At-Least-K-Repeating-Characters.go
@@ -0,0 +1,43 @@
+package leetcode
+
+// Method 1: Divide and Conquer
+func longestSubstring(s string, k int) int {
+	return findLongest(s, 0, len(s), k)
+}
+
+func findLongest(s string, start int, end int, k int) int {
+	// base case: if length of (end - start) < k, already cannot satisfy condition, return 0
+	if end-start < k {
+		return 0
+	}
+
+	// create freq map for each character between s[start : end]
+	count := make([]int, 26)
+	for i := start; i < end; i++ { // O(n)
+		idx := int(s[i] - 'a')
+		count[idx]++
+	}
+
+	// find spliter char in s[start : end]
+	for i := 0; i < 26; i++ { // O(26)
+		if 0 < count[i] && count[i] < k {
+			for j := start; j < end; j++ { // O(n)
+				if int(s[j]) == i+'a' {
+					left := findLongest(s, start, j, k)  // O(n)
+					right := findLongest(s, j+1, end, k) // O(n)
+					return max(left, right)
+				}
+			}
+		}
+	}
+
+	// if jump out of loop, it means all chars >= k
+	return end - start
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ leetcode/0395-Longest-Substring-with-At-Least-K-Repeating-Characters/README.md
@@ -0,0 +1,12 @@
+# [395. Longest Substring with At Least K Repeating Characters](https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/)
+
+## Solution idea
+
+### Divide & Conquer
+
+Time complexity = $O(n^2)$
+
+
+## Resource
+
+- 分治法/递归: [贾考博 LeetCode 395. Longest Substring with At Least K Repeating Characters](https://www.youtube.com/watch?v=Dn_Wt6lBt4I)
