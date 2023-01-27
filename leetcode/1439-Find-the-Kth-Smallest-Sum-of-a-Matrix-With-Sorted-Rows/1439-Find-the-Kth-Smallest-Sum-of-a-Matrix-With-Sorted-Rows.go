a205af8b1a86b3b17a1183c7e5e1a3b0dfdc8dc4
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-01-26 21:55:21 -0600

    add 1439 bin search soln

--- leetcode/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows.go
+++ leetcode/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows.go
@@ -28,3 +28,67 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+// Soln 2: Binary Search 猜答案 -- Optimal Solution
+func kthSmallest(mat [][]int, k int) int {
+	m, n := len(mat), len(mat[0])
+	var left, right int
+	for r := 0; r < m; r++ {
+		left += mat[r][0]
+		right += mat[r][n-1]
+	}
+
+	for left < right { // O(logD)
+		mid := left + (right-left)/2
+		if countMoreOrEqualK(mat, mid, k) { // # of combos whose sum <= mid is >= k
+			right = mid
+		} else { // # of combos whose sum <= mid is < k -> mid 一定不是答案
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func countMoreOrEqualK(mat [][]int, mid int, k int) bool {
+	m := len(mat)
+
+	var sum int
+	for r := 0; r < m; r++ {
+		sum += mat[r][0]
+	}
+	count := 1
+
+	// edge case: smallest sum already > mid
+	if sum > mid {
+		return false
+	}
+
+	dfs(0, mat, mid, k, sum, &count) // O(k)
+
+	if count >= k {
+		return true
+	}
+	return false
+}
+
+func dfs(row int, mat [][]int, target int, k int, sum int, count *int) {
+	m, n := len(mat), len(mat[0])
+
+	// base cases
+	if *count >= k {
+		return
+	}
+	if row >= m {
+		return
+	}
+
+	// recursion
+	for c := 0; c < n; c++ {
+		if sum+mat[row][c]-mat[row][0] <= target {
+			if c > 0 {
+				(*count)++
+			}
+			dfs(row+1, mat, target, k, sum+mat[row][c]-mat[row][0], count)
+		}
+	}
+}
--- leetcode/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows/README.md
+++ leetcode/1439-Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows/README.md
@@ -3,7 +3,15 @@
 ## Solution idea
 
 ### Brute force - Greedy Algorithm
-
-* 逐行
+* `sum` array: 前i行个array的所有可能sum，从小到大排列，前k个最小的sum ($0 \leq i \leq m-1$ where $m$是总行数)
+* 逐行把当前行的每个元素加入`sum` array中；重新排序并且取 top k smallest sums
 
 Time complexity = $O(m * (kn + kn* \log kn)) = O(m*k*n* \log kn)$
+
+
+## Resource
+
+* [【每日一题】1439. Find the Kth Smallest Sum of a Matrix With Sorted Rows, 6/17/2021](https://www.youtube.com/watch?v=y9EzW-S63Vo&ab_channel=HuifengGuan)
+    * Brute force: 0:00 ~ 10:33
+    * k-way merge: 10:43 ~ 32:00
+    * Binary search: 32:01 ~ end
