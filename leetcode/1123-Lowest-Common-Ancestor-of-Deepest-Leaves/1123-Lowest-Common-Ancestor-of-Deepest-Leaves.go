d6a99a2699d100817d36d651eb7b68abed1bbe5a
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-02-05 22:32:33 -0600

    add comments to 1123

--- leetcode/1123-Lowest-Common-Ancestor-of-Deepest-Leaves/1123-Lowest-Common-Ancestor-of-Deepest-Leaves.go
+++ leetcode/1123-Lowest-Common-Ancestor-of-Deepest-Leaves/1123-Lowest-Common-Ancestor-of-Deepest-Leaves.go
@@ -18,6 +18,8 @@ func lcaDeepestLeaves(root *TreeNode) *TreeNode {
 	return res
 }
 
+// return: (LCA, depth)
+// return 的物理意义: subtree中是否找到LCA, 走subtree可以到达的最深有多深
 func helper(root *TreeNode, depth int) (*TreeNode, int) {
 	// base case
 	if root == nil {
@@ -25,14 +27,19 @@ func helper(root *TreeNode, depth int) (*TreeNode, int) {
 	}
 
 	// recursion call
+	// 问问看: 是否能在左子树中找到LCA, 左子树最深可以到达哪里
 	llca, leftDepth := helper(root.Left, depth+1)
-	rlca, rightDepth := helper(root.Right, depth)
+	// 问问看: 是否能在右子树中找到LCA, 右子树最深可以到达哪里
+	rlca, rightDepth := helper(root.Right, depth+1)
 
 	// cur layer return
+
+	// 如果左右子树都能走到的深度一样, 那么当前节点就是它们各自最深叶子节点的最近公共祖先
 	if leftDepth == rightDepth {
-		return root, leftDepth
+		return root, leftDepth // 注意: 这里返回子树上报的depth而不是当前层的depth, 因为子树上报的depth是最深depth
 	}
 
+	// subtree哪个能走得更深就返回哪个
 	if leftDepth > rightDepth {
 		return llca, leftDepth
 	} else {
--- leetcode/1123-Lowest-Common-Ancestor-of-Deepest-Leaves/README.md
+++ leetcode/1123-Lowest-Common-Ancestor-of-Deepest-Leaves/README.md
@@ -4,3 +4,5 @@
 ## Resource
 
 [Solution 1: Get Subtree Height and LCA - Java](https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/solutions/334577/java-c-python-two-recursive-solution/?orderBy=most_votes)
+
+[【每日一题】LeetCode 1123. Lowest Common Ancestor of Deepest Leaves](https://www.youtube.com/watch?v=DUXvcoEZJqw&ab_channel=HuifengGuan)
