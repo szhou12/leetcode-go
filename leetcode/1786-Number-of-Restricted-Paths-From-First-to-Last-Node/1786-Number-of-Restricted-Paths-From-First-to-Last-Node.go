832a106f624eefef49f24e4ed007f5e8dc2b85f6
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-03-13 17:19:45 -0500

    add 1786 code

--- leetcode/1786-Number-of-Restricted-Paths-From-First-to-Last-Node/1786-Number-of-Restricted-Paths-From-First-to-Last-Node.go
+++ leetcode/1786-Number-of-Restricted-Paths-From-First-to-Last-Node/1786-Number-of-Restricted-Paths-From-First-to-Last-Node.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "container/heap"
+
 var M = int(1e9 + 7)
 
 func countRestrictedPaths(n int, edges [][]int) int {
@@ -22,18 +24,94 @@ func countRestrictedPaths(n int, edges [][]int) int {
 	for i := 0; i < len(memo); i++ {
 		memo[i] = -1
 	}
-	return dfs(1, dist, adj, &memo)
+	return dfs(1, n, dist, adj, &memo)
 }
 
-func dfs(cur int, dist []int, adj [][]Pair, memo *[]int) int {
+func dfs(cur int, n int, dist []int, adj [][]Pair, memo *[]int) int {
+	// base case
+	if cur == n {
+		return 1
+	}
 
+	if (*memo)[cur] != -1 {
+		return (*memo)[cur]
+	}
+
+	total := 0
+
+	for _, nei := range adj[cur] {
+		next := nei.node
+		if dist[cur] > dist[next] {
+			total += dfs(next, n, dist, adj, memo)
+			total %= M
+		}
+	}
+	(*memo)[cur] = total
+	return total
 }
 
 func Dijkstra(n int, adj [][]Pair) []int {
+	dist := make([]int, n+1)
+	for i := 0; i < len(dist); i++ {
+		dist[i] = -1
+	}
 
+	minHeap := &PQ{}
+	heap.Init(minHeap)
+
+	// start node = node n
+	heap.Push(minHeap, []int{0, n})
+	// loop
+	for (*minHeap).Len() > 0 {
+		// Pop
+		temp := heap.Pop(minHeap).([]int)
+		d, cur := temp[0], temp[1]
+		// check if already visited
+		if dist[cur] != -1 {
+			continue
+		}
+		dist[cur] = d
+
+		// make the next move
+		for _, nei := range adj[cur] {
+			next, weight := nei.node, nei.weight
+			// check if already visited
+			if dist[next] != -1 {
+				continue
+			}
+			// Push
+			heap.Push(minHeap, []int{d + weight, next})
+		}
+	}
+	return dist
 }
 
 type Pair struct {
 	node   int
 	weight int
+}
+
+type PQ [][]int // [[shortest path, node]]
+
+func (pq PQ) Len() int {
+	return len(pq)
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i][0] < pq[j][0]
+}
+
+func (pq *PQ) Push(x interface{}) {
+	(*pq) = append((*pq), x.([]int))
+}
+
+func (pq *PQ) Pop() interface{} {
+	n := len(*pq)
+	temp := (*pq)[n-1]
+	(*pq) = (*pq)[:n-1]
+	return temp
 }
