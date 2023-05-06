49d251b16a7b1ad088800c122c449860a4d384fd
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-05-06 08:33:05 -0400

    add updates

--- leetcode/1722-Minimize-Hamming-Distance-After-Swap-Operations/1722-Minimize-Hamming-Distance-After-Swap-Operations.go
+++ leetcode/1722-Minimize-Hamming-Distance-After-Swap-Operations/1722-Minimize-Hamming-Distance-After-Swap-Operations.go
@@ -1,3 +1,35 @@
 package leetcode
 
-func minimumHammingDistance(source []int, target []int, allowedSwaps [][]int) int {}
+func minimumHammingDistance(source []int, target []int, allowedSwaps [][]int) int {
+	n := len(source)
+	uf := UnionFind{}
+	uf.Init()
+	for i := 0; i < n; i++ {
+		uf.father[i] = i
+	}
+}
+
+type UnionFind struct {
+	father map[int]int
+}
+
+func (uf *UnionFind) Init() {
+	uf.father = make(map[int]int)
+}
+
+func (uf *UnionFind) Union(X int, y int) {
+	fx := uf.father[x]
+	fy := uf.father[y]
+	if fx < fy {
+		uf.father[fy] = fx
+	} else {
+		uf.father[fx] = fy
+	}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if x != uf.father[x] {
+		uf.father[x] = uf.Find(uf.father[x])
+	}
+	return uf.father[x]
+}
