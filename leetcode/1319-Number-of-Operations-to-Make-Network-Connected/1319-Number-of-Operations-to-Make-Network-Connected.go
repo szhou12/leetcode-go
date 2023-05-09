753d15fcf71ee14d16dee13c9dece52f8d5e6a63
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-05-08 21:49:39 -0500

    add 1319 code

--- leetcode/1319-Number-of-Operations-to-Make-Network-Connected/1319-Number-of-Operations-to-Make-Network-Connected.go
+++ leetcode/1319-Number-of-Operations-to-Make-Network-Connected/1319-Number-of-Operations-to-Make-Network-Connected.go
@@ -1,5 +1,67 @@
 package leetcode
 
 func makeConnected(n int, connections [][]int) int {
+	uf := UnionFind{}
+	uf.Init()
+	for i := 0; i < n; i++ {
+		uf.father[i] = i
+	}
 
+	// Step 1: Union connected components
+	degree := make([]int, n) // in-degree of every node
+	for _, edge := range connections {
+		a, b := edge[0], edge[1]
+		if uf.Find(a) != uf.Find(b) {
+			uf.Union(a, b)
+		}
+		degree[a]++
+		degree[b]++
+	}
+
+	// Step 2: count # of edges & # of nodes for each component
+	// NOTE: each edge is double counted from both ends
+	edges := make(map[int]int)
+	nodes := make(map[int]int)
+	for i := 0; i < n; i++ {
+		root := uf.Find(i)
+		edges[root] += degree[i]
+		nodes[root]++
+	}
+
+	// Step 3: count # of redundant edges for each component
+	redundant := 0
+	for root, cables := range edges {
+		redundant += cables/2 - (nodes[root] - 1)
+	}
+
+	components := len(edges)
+	if redundant >= components-1 {
+		return components - 1
+	}
+	return -1
+}
+
+type UnionFind struct {
+	father map[int]int
+}
+
+func (uf *UnionFind) Init() {
+	uf.father = make(map[int]int)
+}
+
+func (uf *UnionFind) Union(x int, y int) {
+	fx := uf.father[x]
+	fy := uf.father[y]
+	if fx < fy {
+		uf.father[fy] = fx
+	} else {
+		uf.father[fx] = fy
+	}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if x != uf.father[x] {
+		uf.father[x] = uf.Find(uf.father[x])
+	}
+	return uf.father[x]
 }
