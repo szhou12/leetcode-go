191be0e72d52be848fa1041f084a95200515643a
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-06-21 21:52:23 -0500

    add 2662 code

--- leetcode/2662-Minimum-Cost-of-a-Path-With-Special-Roads/2662-Minimum-Cost-of-a-Path-With-Special-Roads.go
+++ leetcode/2662-Minimum-Cost-of-a-Path-With-Special-Roads/2662-Minimum-Cost-of-a-Path-With-Special-Roads.go
@@ -1,15 +1,71 @@
 package leetcode
 
-import "container/heap"
+import (
+	"container/heap"
+	"math"
+)
 
 func minimumCost(start []int, target []int, specialRoads [][]int) int {
 	minHeap := &PQ{}
 	heap.Init(minHeap)
 
-	// Start nodes
+	// Start nodes: start + end node of each special road
 	heap.Push(minHeap, []int{0, encode(start[0], start[1])})
+	
+	// 为什么这么做是正确的? 因为题目给出条件: target node的坐标值 >= 任何一条special road的B点的坐标值。所以可以利用到性质
+	for _, road := range specialRoads { // 物理意义: 不走special road, 直接计算从起点到B点的曼哈顿距离的情况
+		x2, y2 := road[2], road[3]
+		heap.Push(minHeap, []int{manhattanDist(start[0], start[1], x2, y2), encode(x2, y2)})
+	}
 
-	return 0
+	// { node i: shortest path from source to node i }
+	dist := make(map[int]int)
+	// shortest path from source to target
+	res := math.MaxInt
+	// Loop
+	for (*minHeap).Len() > 0 {
+		// Cur
+		temp := heap.Pop(minHeap).([]int)
+		d, cur := temp[0], temp[1]
+		// check if already visited
+		if _, ok := dist[cur]; ok {
+			continue
+		}
+		dist[cur] = d
+		curX, curY := decode(cur)
+		res = min(res, d+manhattanDist(curX, curY, target[0], target[1]))
+
+		// Make the next move
+		for _, road := range specialRoads {
+			x1, y1, x2, y2, cost := road[0], road[1], road[2], road[3], road[4]
+			nei := encode(x2, y2)
+			// check if already visited
+			if _, ok := dist[nei]; ok {
+				continue
+			}
+			heap.Push(minHeap, []int{d + manhattanDist(curX, curY, x1, y1) + cost, nei})
+		}
+	}
+
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func manhattanDist(x1, y1, x2, y2 int) int {
+	return abs(x1-x2) + abs(y1-y2)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 func encode(x, y int) int {
@@ -20,7 +76,7 @@ func decode(n int) (int, int) {
 	return n >> 32, n % (1 << 32)
 }
 
-type PQ [][]int // [[shortest path, node]]
+type PQ [][]int // [[shortest path from source to node, node]]
 
 func (pq PQ) Len() int {
 	return len(pq)
--- leetcode/2662-Minimum-Cost-of-a-Path-With-Special-Roads/README.md
+++ leetcode/2662-Minimum-Cost-of-a-Path-With-Special-Roads/README.md
@@ -3,6 +3,34 @@
 ## Solution idea
 ### Dijkstra
 #### 思路总结
+1. 曼哈顿距离的性质 - 单增坐标情况下曼哈顿距离的计算
+    * 假设: 某路径为 `source -> A -> B -> C -> target`，并且，每个节点的横纵坐标的值**依次单调递增** (i.e. for any node `i < j`, `xi <= xj` and `yi <= yj`)
+    * 那么: `|target - source| = |A - source| + |B-A| + |C-B| + |target - C|`
+```
+(1, 1) -> (3, 5)
+dist = |3 - 1| + |5 - 1| = 2 + 4 = 6
+
+(1, 1) -> (2, 4) -> (3, 5)
+dist = (|2 - 1| + |4 - 1|) + (|3 - 2| + |5 - 4|)
+     = (2 - 1) + (4 - 1) + (3 - 2) + (5 - 4)
+     = (-1, -1) + (2, 4) + (-2, -4) + (3, 5)
+     = (-1, -1) + 0 + (3, 5)
+     = 6
+
+(1, 1) -> (7, 4) -> (3, 5) 不行!
+dist = (|7 - 1| + |4 - 1|) + (|3 - 7| + |5 - 4|)
+     = (7 - 1) + (4 - 1) + (7 - 3) + (5 - 4)
+     = (-1, -1) + (7, 4) + (7, -4) + (-3, 5)
+    != (-1, -1) + (7, 4) + (-7, -4) + (3, 5)
+```
+2. 本题中，从起点`source`到终点`target`的最短距离有两种情况:
+    1. 途中不pick任何一条special road, 纯走曼哈顿距离
+    2. 途中pick一条/多条special road
+3. 上述情况一在实现中需要用到转化思想: 
+    * 题目给出条件: `startX <= x1i, x2i <= targetX`, `startY <= y1i, y2i <= targetY`
+    * 意味着: 横纵坐标值单调递增，就可以利用到上述曼哈顿距离的性质
+    * 代码中: PQ中一开始除了要放入`source`节点, 还要放入从起点到每一个special road末端节点(B点)的曼哈顿距离
+4. Dijkstra Loop 中，第一次Pop `source`，并且expand之后，PQ中就会同时存在从起点到任意一条special road末端节点(B点)的两种情况: 纯曼哈顿距离，pick本条路的距离
 
 ## Resource
 [【每日一题】LeetCode 2662. Minimum Cost of a Path With Special Roads](https://www.youtube.com/watch?v=kQkJzCVQj-o&ab_channel=HuifengGuan)
