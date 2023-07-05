e900db9fbd777793e6f6fdcecdf11f6e6b5518f7
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-07-05 17:50:23 -0500

    finish 1268 code

--- leetcode/1268-Search-Suggestions-System/1268-Search-Suggestions-System.go
+++ leetcode/1268-Search-Suggestions-System/1268-Search-Suggestions-System.go
@@ -16,6 +16,51 @@ func suggestedProducts(products []string, searchWord string) [][]string {
 		}
 		node.isEnd = true
 	}
+
+	res := make([][]string, 0)
+	startNode := root
+	for i := 0; i < len(searchWord); i++ {
+		// 1. tranverse to the node of ith char as the start node
+		prefix := searchWord[:i+1]
+		letter := int(searchWord[i] - 'a')
+		if startNode.children[letter] == nil {
+			for j := i; j < len(searchWord); j++ {
+				res = append(res, []string{})
+			}
+			break
+		}
+		startNode = startNode.children[letter]
+
+		// 2. use DFS to fetch first 3 words that begin with the start node
+		words := make([]string, 0)
+		dfs(startNode, prefix, &words)
+		res = append(res, words)
+		
+	}
+
+	return res
+}
+
+func dfs(node *TrieNode, prefix string, res *[]string) {
+	// base case: if already found 3 words, return
+	if len(*res) == 3 {
+		return
+	}
+
+	if node.isEnd {
+		*res = append(*res, prefix)
+		// Do NOT return since we may be able to traverse deeper
+	}
+
+	for i := 0; i < 26; i++ {
+		if node.children[i] != nil {
+			prefix += string('a' + i)
+			dfs(node.children[i], prefix, res)
+			prefix = prefix[:len(prefix)-1]
+		}
+	}
+
+
 }
 
 type TrieNode struct {
