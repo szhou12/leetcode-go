a5ec5cdacc3f1a7b369e458e522c18a136b4f3cd
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-07-09 20:33:52 -0500

    add 140 code lacking DFS

--- leetcode/0140-Word-Break-II/0140-Word-Break-II.go
+++ leetcode/0140-Word-Break-II/0140-Word-Break-II.go
@@ -1,7 +1,28 @@
 package leetcode
 
 func wordBreak(s string, wordDict []string) []string {
+	 root := newTrieNode()
+	 // Step 1: build Trie from wordDict
+	 for _, word := range wordDict {
+		node := root
+		for _, char := range word {
+			letter := int(char - 'a')
+			if node.children[letter] == nil {
+				node.children[letter] = newTrieNode()
+			}
+			node = node.children[letter]
+		}
+		node.isEnd = true
+	 }
 
+	 // Step 2: DFS
+	 var res []string
+	 dfs(s, root, 0, "", &res)
+	 return res
+}
+
+func dfs(s string, root *TrieNode, startIndex int, sentence string, res *[]string) {
+	
 }
 
 type TrieNode struct {
