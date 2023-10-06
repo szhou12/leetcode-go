63e7571913367ed2344f2a8f04c2834d61cc809e
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-10-05 21:59:28 -0500

    add 0028 KMP not complete

--- leetcode/0028-Implement-strStr/0028-Implement-strStr-KMP.go
+++ leetcode/0028-Implement-strStr/0028-Implement-strStr-KMP.go
@@ -1,5 +1,47 @@
 package leetcode
 
 func strStr_KMP(haystack string, needle string) int {
-	
+	n := len(haystack)
+	m := len(needle)
+	// edge cases
+	if m == 0 {
+		return 0
+	}
+	if n == 0 {
+		return -1
+	}
+
+	// build lsp for pattern string = needle
+	lsp := preprocess(needle)
+
+	// dp[i] = max length j of suffix haystack[:i] s.t. prefix needle[0:j-1] == suffix haystack[i-j+1:i] (双闭区间)
+	dp := make([]int, n)
+	// base case
+	dp[0] = 0
+
+}
+
+func preprocess(needle string) []int {
+	n := len(needle)
+
+	// dp[i] = max length j s.t. prefix needle[0:j-1] == suffix needle[i-j+1:i] (双闭区间)
+	dp := make([]int, n)
+
+	// base case
+	dp[0] = 0
+
+	// recurrence
+	for i := 1; i < n; i++ {
+		j := dp[i-1]
+		for j>=1 && needle[i] != needle[j] {
+			j = dp[j-1]
+		}
+		if needle[i] == needle[j] {
+			dp[i] = j + 1
+		} else {
+			dp[i] = j
+		}
+	}
+
+	return dp
 }
