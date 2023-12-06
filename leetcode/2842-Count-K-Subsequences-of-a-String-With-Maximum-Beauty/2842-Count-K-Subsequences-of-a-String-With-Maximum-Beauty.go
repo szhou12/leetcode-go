17df1b66b4be0a36dfff7ccd9ac3427a50ab9c3d
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2023-12-05 19:16:55 -0600

    add 2842 code

--- leetcode/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty.go
+++ leetcode/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty.go
@@ -1,7 +1,77 @@
 package leetcode
 
-var M = int(1e9+7)
+import "sort"
+
+var M = int(1e9 + 7)
 
 func countKSubsequencesWithMaxBeauty(s string, k int) int {
-	
+	f := make(map[byte]int)
+	for i := 0; i < len(s); i++ { // O(n)
+		f[s[i]]++
+	}
+	counter := make([]int, 0)
+	for _, v := range f { // O(26)
+		counter = append(counter, v)
+	}
+	// early return: if total number of unique characters < k, return 0
+	if len(counter) < k {
+		return 0
+	}
+
+	// sort in decreasing order to get Top K
+	sort.Slice(counter, func(i, j int) bool { // O(26log26)
+		return counter[i] > counter[j]
+	})
+	// target beauty = sum(top k)
+	targetBeauty := 0
+	for i := 0; i < k; i++ { // O(k)
+		targetBeauty += counter[i]
+	}
+
+	// DFS
+	// total number of subsequences that satisfy the condition
+	total := 0
+
+	// curChoices: current numeber of subsequences that satisfy the condition
+	// at start, curChoinces = 1 cause choosing nothing is one option
+	dfs(k, 0, 0, 0, targetBeauty, counter, 1, &total) // O(2^k) where k <= 26
+
+	return total
+
+}
+
+func dfs(k int, curIndex int, picked int, curBeauty int, targetBeauty int, counter []int, curChoices int, total *int) {
+	// base cases
+	if picked == k && curBeauty == targetBeauty {
+		*total += curChoices
+		*total %= M
+		return
+	}
+
+	if curBeauty > targetBeauty {
+		return
+	}
+
+	if picked > k {
+		return
+	}
+
+	// pruning
+	if curBeauty + sum(counter[curIndex:]) < targetBeauty {
+		return
+	}
+
+	// recursion
+	for i := curIndex; i < len(counter); i++ {
+		// NOTE: (curChoices*counter[i]) MUST mod M !
+		dfs(k, i+1, picked+1, curBeauty+counter[i], targetBeauty, counter, curChoices*counter[i]%M, total)
+	}
+}
+
+func sum(arr []int) int {
+	res := 0
+	for _, count := range arr {
+		res += count
+	}
+	return res
 }
--- leetcode/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty/README.md
+++ leetcode/2842-Count-K-Subsequences-of-a-String-With-Maximum-Beauty/README.md
@@ -3,7 +3,15 @@
 ## Solution idea
 ### DFS
 
+#### 优化解法
+* 用一个 array `counter[]` 记录每一个字母在输入字符串 `s` 中出现的次数 (`counter[]` 最长只有26)
+* 将 `counter[]` 从大到小排序，前k个的和 就是能得到的最大beauty
+* 再在排好序的 `counter[]` 上进行 DFS。每一次选择加入当前字母时，当前subsequence组合数就乘上加入字母对应的次数 (因为是从有次数个相同字母中选择一个)
+* Time complexity = $O(n) + O(26) + O(26\log 26) + O(k) + O(26^k)$
+
 #### 我的解法 (会超时:exclamation:)
+* 不做任何变换，直接在输入字符串 `s` 上进行 DFS 穷举出所有符合要求的子序列
+* Time complexity = $O(n^k)$
 ```go
 var M = int(1e9+7)
 
@@ -61,7 +69,6 @@ func dfs(s string, k int, cur string, curIndex int, visited []bool, f []int, bea
 }
 ```
 
-#### 优化解法
 
 ## Resource
 [【每日一题】LeetCode 2842. Count K-Subsequences of a String With Maximum Beauty](https://www.youtube.com/watch?v=8pPPcODAWAA)
