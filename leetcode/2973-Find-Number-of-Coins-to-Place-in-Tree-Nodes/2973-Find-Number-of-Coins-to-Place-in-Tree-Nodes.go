ec5da018ac28132d9aa99eee7c95c4efe6b81fe6
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-01-08 23:02:56 -0600

    add 2973 code

--- leetcode/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes.go
+++ leetcode/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes.go
@@ -46,11 +46,39 @@ func dfs(cur int, parent int, next [][]int, cost []int, subtree [][]int, res []i
 
 	arr = append(arr, cost[cur])
 	// slices.Sort() is the new feature in go 1.21. Faster then sort.Ints()
-	slices.Sort(arr)
+	slices.Sort(arr) // increasing order
 	n := len(arr)
 
-	
+	// update res[cur]
+	if n < 3 {
+		res[cur] = 1
+	} else {
+		cand1 := int64(arr[n-3] * arr[n-2] * arr[n-1])
+		cand2 := int64(arr[0] * arr[1] * arr[n-1])
+		res[cur] = max(0, max(cand1, cand2))
+	}
 
-	
-
+	// assemble subtree[cur]
+	// arr at least length 1 so to have the smallest negative
+	if n >= 1 {
+		subtree[cur] = append(subtree[cur], arr[0])
+	}
+	// arr at least length 2 so to have the 2nd smallest negative without overlapping with the smallest negative
+	if n >= 2 {
+		subtree[cur] = append(subtree[cur], arr[1])
+	}
+	// arr at least length 5 so to have the 3rd largest positive without overlapping with 2 smallest negatives
+	// i.e. 如果只有4个元素，第三大就是第二小，就是在重复添加
+	// [ X X p p p ]
+	if n >= 5 {
+		subtree[cur] = append(subtree[cur], arr[n-3])
+	}
+	// arr at least length 4 so to have the 2nd largest positive without overlapping with 2 smallest negatives
+	if n >= 4 {
+		subtree[cur] = append(subtree[cur], arr[n-2])
+	}
+	// arr at least length 3 so to have the larest positive without overlapping with 2 smallest negatives
+	if n >= 3 {
+		subtree[cur] = append(subtree[cur], arr[n-1])
+	}
 }
--- leetcode/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes/README.md
+++ leetcode/2973-Find-Number-of-Coins-to-Place-in-Tree-Nodes/README.md
@@ -1,5 +1,9 @@
 # [2973. Find Number of Coins to Place in Tree Nodes](https://leetcode.com/problems/find-number-of-coins-to-place-in-tree-nodes/description/)
 
 ## Solution idea
+### DFS + Sorting
+
+
 
 ## Resource
+[【每日一题】LeetCode 2973. Find Number of Coins to Place in Tree Nodes](https://www.youtube.com/watch?v=Omfwj5wARUA&ab_channel=HuifengGuan)
