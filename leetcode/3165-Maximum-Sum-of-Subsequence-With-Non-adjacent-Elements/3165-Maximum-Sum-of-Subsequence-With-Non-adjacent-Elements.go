7f93afd60841890313ea1687324f55f5c2946c96
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-05-30 19:03:06 -0500

    updated segmenttree template

---
+++ leetcode/3165-Maximum-Sum-of-Subsequence-With-Non-adjacent-Elements/3165-Maximum-Sum-of-Subsequence-With-Non-adjacent-Elements.go
@@ -0,0 +1,6 @@
+package leetcode
+
+var M = int(1e9+7)
+func maximumSumSubsequence(nums []int, queries [][]int) int {
+
+}
--- template/SegmentTree.go
+++ template/SegmentTree.go
@@ -3,12 +3,13 @@ package template
 type SegTreeNode struct {
 	left, right *SegTreeNode
 	start, end  int
-	info        int  // sum over the range
+	info        int  // stored value over the range [start, end]. e.g. stored value can be sum([a:b]), max([a:b]), min([a:b]), etc.
 	delta       int  // Used for lazy propagation, it stores the value that needs to be added to all elements in this node's range
 	tag         bool // A flag to indicate whether this node has a pending update (delta) that needs to be propagated to its children
 }
 
 /*
+Constructor: 所有叶子节点的info初始化为单一值val
 Create a new segment tree node and recursively build the entire segment tree
 */
 func NewSegTreeNode(start, end int, val int) *SegTreeNode {
@@ -24,11 +25,12 @@ func NewSegTreeNode(start, end int, val int) *SegTreeNode {
 	mid := (start + end) / 2
 	node.left = NewSegTreeNode(start, mid, val)
 	node.right = NewSegTreeNode(mid+1, end, val)
-	node.info = node.left.info + node.right.info // what is it doing?
+	node.info = node.left.info + node.right.info // node's info stores the sum of the range [a,b]
 	return node
 }
 
 /*
+更新<lazy标记>
 This method is used for lazy propagation. When a node has an unpropagated update (tag==true), this method applies the update to its children.
 1. The info of each child is increased by delta times the size of the range it covers.
 2. The delta is added to each child's delta.
@@ -51,13 +53,14 @@ func (node *SegTreeNode) pushDown() {
 }
 
 /*
+区间修改
 This method updates a range [a, b] in the segment tree by adding val to each element in the range.
  1. If the current node's range is completely outside [a, b], it does nothing.
  2. If the current node's range is completely inside [a, b], it updates info and sets delta and tag for lazy propagation.
  3. If the current node's range partially overlaps with [a, b], it first propagates any pending updates to its children (using pushDown()),
     then recursively updates its children, and finally updates its info based on its children's info.
 */
-func (node *SegTreeNode) updateRangeBy(start int, end int, val int) {
+func (node *SegTreeNode) updateRange(start int, end int, val int) {
 	if end < node.start || node.end < start {
 		return
 	}
@@ -68,12 +71,13 @@ func (node *SegTreeNode) updateRangeBy(start int, end int, val int) {
 		return
 	}
 	node.pushDown()
-	node.left.updateRangeBy(start, end, val)
-	node.right.updateRangeBy(start, end, val)
+	node.left.updateRange(start, end, val)
+	node.right.updateRange(start, end, val)
 	node.info = node.left.info + node.right.info
 }
 
 /*
+区间查询
 This method queries the sum of elements in the range [a, b].
 1. If the current node's range is completely outside [a, b], it returns 0
 2. If the current node's range is completely inside [a, b], it returns the node's info.
