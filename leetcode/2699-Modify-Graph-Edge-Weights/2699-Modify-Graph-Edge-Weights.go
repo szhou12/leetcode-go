d20ba79df54ec958b52b8675538f41113433d05f
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-06-11 04:10:17 -0500

    modified 2699

--- leetcode/2699-Modify-Graph-Edge-Weights/2699-Modify-Graph-Edge-Weights.go
+++ leetcode/2699-Modify-Graph-Edge-Weights/2699-Modify-Graph-Edge-Weights.go
@@ -7,21 +7,21 @@ import (
 
 func modifiedGraphEdges(n int, edges [][]int, source int, destination int, target int) [][]int {
 	// Step 1: Reconstruct adj-list repr + Modify -1 -> 1
-	next := make([]map[int]int, n) // [{to, weight}]
+	next := make([]map[int]int, n) // next[cur] = {nei_1: weight_1, nei_2: weight_2}
 	for i := 0; i < n; i++ {
 		next[i] = make(map[int]int)
 	}
-	// mark edges with negative weights
-	negEdges := make([]map[int]int, n) // [{to, weight}]
+	// mark modifiable edges
+	modify := make([]map[int]bool, n) // modify[cur] = {nei_1: true, nei_2: true}
 	for i := 0; i < n; i++ {
-		negEdges[i] = make(map[int]int)
+		modify[i] = make(map[int]bool)
 	}
 	for _, edge := range edges {
 		u, v, weight := edge[0], edge[1], edge[2]
 		if weight == -1 {
 			weight = 1
-			negEdges[u][v] = 1
-			negEdges[v][u] = 1
+			modify[u][v] = true
+			modify[v][u] = true
 		}
 		next[u][v] = weight
 		next[v][u] = weight
@@ -30,7 +30,7 @@ func modifiedGraphEdges(n int, edges [][]int, source int, destination int, targe
 	minHeap := &PQ{}
 	heap.Init(minHeap)
 
-	// Step 2: Dijkstra: dest -> each node
+	// Step 2: Dijkstra: destination -> each node
 	dist2 := make([]int, n)
 	for i := 0; i < n; i++ {
 		dist2[i] = math.MaxInt / 3
@@ -46,6 +46,8 @@ func modifiedGraphEdges(n int, edges [][]int, source int, destination int, targe
 		if dist2[cur] != math.MaxInt/3 {
 			continue
 		}
+
+		// Upddate
 		dist2[cur] = d
 
 		// Make the next move
@@ -74,8 +76,13 @@ func modifiedGraphEdges(n int, edges [][]int, source int, destination int, targe
 		if dist1[cur] != math.MaxInt/3 {
 			continue
 		}
+
+		// Update
 		dist1[cur] = d
-		// check if arrive at destination & path cost == target
+
+		// check if when arrive at destination, path cost == target
+		// 如果到达destination时，最短路径的cost还不等于target，说明这条最短路径不途径任何一条modifiable edge
+		// 因为在每次进PQ时，我们都已经提前修改了modifiable edge的边权，保证只要通路上途径它，它一定不是造成path cost != target的凶手之一。
 		if cur == destination && d != target {
 			return [][]int{}
 		}
@@ -87,13 +94,13 @@ func modifiedGraphEdges(n int, edges [][]int, source int, destination int, targe
 				continue
 			}
 			// increase edge weight if < target
-			if negEdges[cur][nei] == 1 && dist1[cur] + weight + dist2[nei] < target {
+			if modify[cur][nei] && dist1[cur] + weight + dist2[nei] < target {
 				weight = target - dist1[cur] - dist2[nei]
 				// update adj-list
 				next[cur][nei] = weight
 				next[nei][cur] = weight
 			}
-			heap.Push(minHeap, []int{d + weight, nei})
+			heap.Push(minHeap, []int{d + weight, nei}) // 关键点: 下一次pop时，不一定这个调整过边权的nei是堆里的最小值，也有可能到达其他nei(cur -> nei是不可修改边)的最短距离更小，从而在下一次pop时被选中弹出。所以，要贪心地反复调整之后弹出的节点，它的nei出现modifiable edge的权重。从而“堵死”从起点到终点的最短路径上，经过modifiable edge，竟然path cost还不等于target的情况。也就是说，这条完整的最短路径，你要不然不途径任何一条modifiable edge，行，你可以有path cost != target；否则只要途径哪怕一条modifiable edge, 这个贪心思想都确保了path cost == target
 		}
 	}
 
@@ -107,7 +114,7 @@ func modifiedGraphEdges(n int, edges [][]int, source int, destination int, targe
 
 }
 
-type PQ [][]int // [[shortest distance, node]]
+type PQ [][]int // [[shortest distance from start -> node, node]]
 
 func (pq PQ) Len() int {
 	return len(pq)
--- leetcode/2699-Modify-Graph-Edge-Weights/README.md
+++ leetcode/2699-Modify-Graph-Edge-Weights/README.md
@@ -3,6 +3,7 @@
 ## Solution idea
 ### Dijkstra
 #### 思路总结
+1. 题目要求所有权重为-1的边一定要修改成正数。所以，第一步先把所有负数边的权重改成最小正数1，跑一遍Dijkstra，看看最短路径的总消耗是否等于target。如果 > target，显然找不到任何可行解；只有 <= target才可能存在解。但是，当 < target时，同时还需要满足最短路径途经至少一条“修改”边，否则也不存在可行解。
 
 ## Resource
 [【每日一题】LeetCode 2699. Modify Graph Edge Weights](https://www.youtube.com/watch?v=gchifH4Ftjc&list=PLwdV8xC1EWHrtgsYCcDTXIMVaHSlsnLzL&index=14&ab_channel=HuifengGuan)
