62ced76f3b5dea4c343a63900f6bf9064b0703c2
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-06-20 16:50:36 -0500

    added 3176 code

---
+++ leetcode/3176-Find-the-Maximum-Length-of-a-Good-Subsequence-I/3176-Find-the-Maximum-Length-of-a-Good-Subsequence-I.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func maximumLength(nums []int, k int) int {
+	n := len(nums)
+
+	// dp[i][t] := maximum possible length of a good subsequence of nums[0:i] including i-th element && there costs t times of seq[i] != seq[i + 1]
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, k+1)
+	}
+
+	// base case: dp[0][0...k] = 1 all covered in recurrence
+
+	// recurrence
+	res := 1
+	for i := 0; i < n; i++ {
+		for t := 0; t <= k; t++ {
+
+			cur := 1 // trick: for every i, 自立门户，最小可能是i号元素本身，所以长度为1
+
+			for j := 0; j < i; j++ {
+				if nums[j] == nums[i] {
+					cur = max(cur, dp[j][t]+1)
+				} else if t >= 1 {
+					cur = max(cur, dp[j][t-1]+1)
+				}
+			}
+
+			dp[i][t] = cur
+			res = max(res, dp[i][t]) // res在每一次i都要更新, 因为不一定i==n-1时是最大值
+		}
+	}
+
+	return res
+}
---
+++ leetcode/3176-Find-the-Maximum-Length-of-a-Good-Subsequence-I/README.md
@@ -0,0 +1,7 @@
+# [3176. Find the Maximum Length of a Good Subsequence I](https://leetcode.com/problems/find-the-maximum-length-of-a-good-subsequence-i/description/)
+
+## Solution idea
+### 2-D DP
+
+## Resource
+[【每日一题】LeetCode 3177. Find the Maximum Length of a Good Subsequence II](https://www.youtube.com/watch?v=lEOzA3KXV5s&t=83s&ab_channel=HuifengGuan) (0:00 ~ 12:23)
