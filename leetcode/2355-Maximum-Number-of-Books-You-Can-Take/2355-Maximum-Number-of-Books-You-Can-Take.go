bccd84cb83d9475a2a7427a3aa00904846debcca
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2025-02-07 22:01:40 -0600

    added 2355 soln 2 - prevSmaller

--- leetcode/2355-Maximum-Number-of-Books-You-Can-Take/2355-Maximum-Number-of-Books-You-Can-Take.go
+++ leetcode/2355-Maximum-Number-of-Books-You-Can-Take/2355-Maximum-Number-of-Books-You-Can-Take.go
@@ -5,13 +5,11 @@ func maximumBooks(books []int) int64 {
 	n := len(books)
 	dp := make([]int, n)
 
-	// base case
-	dp[0] = books[0]
 
 	stack := make([]int, 0) // stores index
 
 	// recurrence
-	for i := 1; i < n; i++ {
+	for i := 0; i < n; i++ {
 		for len(stack) > 0 && books[stack[len(stack)-1]] > books[i]-(i-stack[len(stack)-1]) {
 			stack = stack[:len(stack)-1]
 		}
@@ -21,8 +19,8 @@ func maximumBooks(books []int) int64 {
 			L := i - j
 			dp[i] = dp[j] + ((books[i]-L+1)+books[i])*L/2
 		} else {
-			L := min(i + 1, books[i])
-			dp[i] = ((books[i]-L+1)+books[i])*L/2
+			L := min(i+1, books[i])
+			dp[i] = ((books[i] - L + 1) + books[i]) * L / 2
 		}
 
 		stack = append(stack, i)
@@ -34,4 +32,61 @@ func maximumBooks(books []int) int64 {
 	}
 	return int64(res)
 
+}
+
+func maximumBooks2(books []int) int64 {
+	n := len(books)
+
+	prevSmaller := findPrevSmaller(books)
+
+	dp := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		j := prevSmaller[i]
+		L := min(books[i], i-j)
+		arithmeticSum := (books[i] + (books[i] - L + 1)) * L / 2
+
+		dp[i] = arithmeticSum
+		if j != -1 {
+			dp[i] += dp[j]
+		}
+	}
+
+	res := 0
+	for i := 0; i < n; i++ {
+		res = max(res, dp[i])
+	}
+	return int64(res)
+
+}
+
+func findPrevSmaller(books []int) []int {
+	n := len(books)
+
+	// preprocessing
+	// 0  1  2
+	//[5, 3, 7]
+	//[5, 2, 5]
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		nums[i] = books[i] - i
+	}
+
+	// monotonic increasing stack
+	stack := make([]int, 0)
+
+	prevSmaller := make([]int, n)
+	for i := 0; i < n; i++ {
+		prevSmaller[i] = -1
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) > 0 && nums[stack[len(stack)-1]] > nums[i] {
+			prevSmaller[stack[len(stack)-1]] = i
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+
+	return prevSmaller
 }
--- leetcode/2355-Maximum-Number-of-Books-You-Can-Take/README.md
+++ leetcode/2355-Maximum-Number-of-Books-You-Can-Take/README.md
@@ -2,6 +2,26 @@
 
 ## Solution idea
 ### DP + Stack
+1. 见到Subarray的题，首先想想有哪些常用的算法？能想到的有: DP, Stack, Prefix Sum, Sliding Window, Segment Tree
+    - DP: 能想到的是定义 `dp[i] := ` max subarray sum ending at i.
+    - Stack: 能想到的是用单调栈 given `nums[i]`，找 prevSmaller, nextSmaller etc.
+2. observation: 我们留意到一个性质 - 看`dp[i]`时，势必我们会把`books[i]`都取满。
+3. 接下来的问题是，基于这个observation，我们怎么向前取，得到最大的subarray？我们考虑下面的例子：
+    ```
+                i
+    ... 3 10 10 9
+    ... 3][7 8] 9
+    ```
+    站在`i`处，如果前面的值更大，那我们没法取满，只能逐次递减地取，从而形成等差数列；如果前面的值更小，陡减，没法形成一个等差数列，这时候，我们可以取满，而取满就意味着我们向前遇到了`dp[j]`。这样，我们就发现，`dp[i]`代表的subarray可以大概分成两部分: `dp[j] + sum of arithmetic seq`
+4. 好，发现这个pattern以后，我们就可以考虑怎么确定这个分界点`j`。发现没有，找这个`j`的过程，其实很像用单调递增栈找prevSmaller。只是，我们不能单纯地套用模版。因为，我们不能单纯地用`nums[i]`去找prevSmaller。比如下面这个例子:
+    ```
+                i
+    2, 9, 4, 7, 5, 2
+          ^
+    1  2  3  4  5
+    ```
+    当在5这里时，它的prevSmaller是4，但是4并不是分界点`j`，我们没法取满4。所以，我们要额外留意，什么时候算是“前面的值更小”？
+5. 答案是预留一段等差数列的空间之后，还小。即: 我们不用`nums[i]`本身去比较，而是做一个变换，使用`nums[i] - i`。
 
 Time complexity = $O(n)$
 
